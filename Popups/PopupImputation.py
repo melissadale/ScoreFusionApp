3010701082f3a4d7307c1e40bd7d60e23d8f065f
melissa_dale <melissa.r.dale@gmail.com>
2021-06-09 11:28:19 -0400

    Initial popup completed. TODO: hook up popup functionality.

---
+++ Popups/PopupImputation.py
@@ -0,0 +1,20 @@
+import kivy
+from kivy.uix.gridlayout import GridLayout
+kivy.require('1.9.0')
+
+class Imputation(GridLayout):
+    dpop = None
+    imputation = "ignore"
+
+
+    def set_imputation(self, imp):
+        self.imputation = imp
+
+    def set_pop(self, pwin):
+        self.dpop = pwin
+
+    def close(self):
+        self.dpop.dismiss()
+
+    def get_imputation(self):
+        return self.imputation
